65d17a5a6536fd791f7a325a0628db19e2876925
andrelsrn <andrel.srn@hotmail.com>
2025-10-28 21:27:50 -0300

    finbByDepartment implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,32 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1: seller findById ===");
-
+        System.out.println("===== TEST 1: seller findById =====");
         Seller seller = sellerDao.findById(2);
-        
-
         System.out.println(seller);
+        System.out.println();
+
+        System.out.println("===== TEST 2: seller findByDepartment =====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj: list){
+            System.out.println(obj);
+        }
+
+
+
+
+
 
     }
 }
